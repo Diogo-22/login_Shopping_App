cbd9df347a95824a43d960375b01bc2c02b0a733
Diogo <diogo.peralta.goncalves@gmail.com>
2023-09-15 22:12:43 +0100

    pls

--- src/components/PersistLogin.js
+++ src/components/PersistLogin.js
@@ -3,12 +3,15 @@ import { useState, useEffect } from "react";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
 import useLocalStorage from "../hooks/useLocalStorage";
+import Home from "./Home";
+import { useNavigation } from "react-router-dom";
 
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
   const { auth } = useAuth();
   const [persist] = useLocalStorage("persist", false);
+  const navigate = useNavigation();
 
   useEffect(() => {
     const verifyRefreshToken = async () => {
@@ -19,6 +22,7 @@ const PersistLogin = () => {
         console.error(err);
       } finally {
         setIsLoading(false);
+        navigate(Home);
       }
     };
     !auth.accessToken ? verifyRefreshToken() : setIsLoading(false);
