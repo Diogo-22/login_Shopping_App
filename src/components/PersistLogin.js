0a096a8594c811633b0364b66f05889861f6a26d
Diogo Goncalves <diogo.peralta.goncalves@tecnico.ulisboa.pt>
2023-09-30 12:52:24 +0100

    fix infinite loop

--- src/components/PersistLogin.js
+++ src/components/PersistLogin.js
@@ -5,29 +5,42 @@ import useAuth from "../hooks/useAuth";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Home from "./Home";
 import { useNavigate } from "react-router-dom";
+import Login from "./Login";
 
 const PersistLogin = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const refresh = useRefreshToken();
-  const { auth } = useAuth();
+  const { auth, stop, setStop } = useAuth();
   const [persist] = useLocalStorage("persist", false);
   const navigate = useNavigate();
+  /* console.log("its looping");
+  console.log(stop); */
 
   useEffect(() => {
     const verifyRefreshToken = async () => {
-      try {
-        console.log("going to try refreshing");
-        setIsLoading(false);
-        await refresh();
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setIsLoading(false);
-        navigate(Home);
+      //console.log(isLoading);
+
+      if (stop == false) {
+        try {
+          console.log("going to try refreshing");
+          setIsLoading(false);
+          await refresh();
+        } catch (err) {
+          //console.error(err);
+          setStop(true);
+          setIsLoading(false);
+        } finally {
+          setIsLoading(false);
+          navigate(Home);
+        }
       }
     };
-    !auth.accessToken ? verifyRefreshToken() : setIsLoading(false);
-  }, []);
+    if (!auth.accessToken) {
+      verifyRefreshToken();
+    } else {
+      setIsLoading(false);
+    }
+  }, [auth.accessToken, stop]);
 
   return (
     <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -5,11 +5,14 @@ const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
   const [location, setLocation] = useState("");
+  const [stop, setStop] = useState(false);
 
   //const [persist, setPersist] = useState(JSON.parse(localStorage.getItem('persist')) || false)
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth, location, setLocation }}>
+    <AuthContext.Provider
+      value={{ auth, setAuth, location, setLocation, stop, setStop }}
+    >
       {children}
     </AuthContext.Provider>
   );
